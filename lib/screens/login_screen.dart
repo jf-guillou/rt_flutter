3606cf0123b4f74187d1a74fc80ee5486b844246
jf-guillou <jean.f.guillou@gmail.com>
2023-02-17 22:13:49 +0100

    Libs update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rt_flutter/screens/home_screen.dart';
@@ -15,13 +14,46 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
+  final WebViewController controller = WebViewController();
 
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel("TokenExtractor", onMessageReceived: (message) {
+        String token = message.message;
+        Provider.of<AppState>(context, listen: false).token = token;
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => HomeScreen()));
+      })
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            print("WebView is loading (progress : $progress%)");
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+            controller.currentUrl().then((value) async {
+              if (hasReachedTokensList(value)) {
+                String owner = await getUserID(controller);
+                PackageInfo packageInfo = await PackageInfo.fromPlatform();
+                String description =
+                    packageInfo.packageName + "_" + packageInfo.version;
+                generateToken(controller, owner, description);
+              }
+            });
+          },
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(tokenListUrl());
   }
 
   @override
@@ -33,51 +65,21 @@ class _LoginScreenState extends State<LoginScreen> {
       // We're using a Builder here so we have a context that is below the Scaffold
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
-        return WebView(
-          initialUrl: tokenListUrl(),
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          javascriptChannels: [
-            JavascriptChannel(
-                name: 'TokenExtractor',
-                onMessageReceived: (message) {
-                  String token = message.message;
-                  Provider.of<AppState>(context, listen: false).token = token;
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                })
-          ].toSet(),
-          onProgress: (int progress) {
-            print("WebView is loading (progress : $progress%)");
-          },
-          onPageStarted: (String url) {
-            print('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            print('Page finished loading: $url');
-            _controller.future.then(
-                (controller) => controller.currentUrl().then((value) async {
-                      if (hasReachedTokensList(value)) {
-                        String owner = await getUserID(controller);
-                        PackageInfo packageInfo =
-                            await PackageInfo.fromPlatform();
-                        String description =
-                            packageInfo.packageName + "_" + packageInfo.version;
-                        generateToken(controller, owner, description);
-                      }
-                    }));
-          },
-          gestureNavigationEnabled: true,
-        );
+        return WebViewWidget(controller: controller);
       }),
     );
   }
 
-  String tokenListUrl() {
-    return APIService.instance.config!.uri.toString() +
-        '/Prefs/AuthTokens.html';
+  Uri tokenListUrl() {
+    Uri? uri = APIService.instance.config!.uri;
+    if (uri == null) {
+      throw "Missing base uri";
+    }
+
+    uri.pathSegments.add("Prefs");
+    uri.pathSegments.add("AuthTokens.html");
+
+    return uri;
   }
 
   bool hasReachedTokensList(String? url) {
@@ -85,14 +87,16 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Future<String> getUserID(WebViewController controller) async {
-    return controller.runJavascriptReturningResult('RT.CurrentUser.id');
+    return controller
+        .runJavaScriptReturningResult('RT.CurrentUser.id')
+        .then((value) => value.toString());
   }
 
   void generateToken(
       WebViewController controller, String ownerID, String description) {
     String params = jsonEncode(
         {"Owner": ownerID, "Description": description, "Create": true});
-    controller.runJavascript(
+    controller.runJavaScript(
         'fetch("${tokenListUrl()}", { method: "POST", body: new URLSearchParams($params) }).then((response) => { ' +
             'if (response.ok) { return response.text().then((text) => { ' +
             'window.TokenExtractor.postMessage(text.match("(\\\\d+-\\\\d+-\\\\w+)")[0]) }) }})');
